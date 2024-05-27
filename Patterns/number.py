6105aeb7d014295ad4353654508e0aef71bb729f
Vidhi <srivastavavidhi29@gmail.com>
2024-05-27 15:31:25 +0530

    Pattern questions

--- Patterns/number.py
+++ Patterns/number.py
@@ -2,7 +2,22 @@ print("Enter length: ")
 n = int(input())
 
 # Right-Angled Number Pyramid
+print("Right-Angled Number Pyramid\n")
 for i in range(1, n+1):
     for j in range(1, i+1):
         print(j, end="")
+    print("\n")
+
+# Right-Angled Number Pyramid - II
+print("Right-Angled Number Pyramid - II\n")
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(i, end="")
+    print("\n")
+
+#Inverted Numbered Right Pyramid
+print("Inverted Numbered Right Pyramid\n")
+for i in range(n, 0, -1):
+    for j in range(i):
+        print(j+1, end="")
     print("\n")
--- Patterns/star.py
+++ Patterns/star.py
@@ -2,13 +2,33 @@ print("Enter length: ")
 n = int(input())
 
 # rectangular Star pattern
+print("rectangular Star pattern\n")
 for i in range(n):
     for j in range(n):
         print("* ", end="")
     print("\n")
 
 # Right-Angled Triangle Pattern
+print("Right-Angled Triangle Pattern")
 for i in range(n):
     for j in range(i):
         print("* ", end="")
     print("\n")
+
+# Inverted Right Pyramid
+print("Inverted Right Pyramid\n")
+for i in range(n, 0, -1):
+    for j in range(i):
+        print("* ", end="")
+    print("\n")
+
+# Star Pyramid
+print("Star Pyramid\n")
+for i in range(n):
+    for j in range(i):
+        if(j % 2 != 0):
+            print("* ", end="")
+        else:
+            print(" ", end="")
+    print("\n")
+
